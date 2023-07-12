19faf25daec4acfb4751032bbc8439cb1420928d
elvisbevab <elvis.bevab@mailbox.org>
2023-07-12 17:37:44 +0200

    hover effects

--- client/src/components/buttons/PrimaryBtn.jsx
+++ client/src/components/buttons/PrimaryBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PrimaryBtn() {
   return (
-    <button className='bg-skin-button-primary text-skin-primary text-2xl font-bold my-2 py-3 px-10 rounded-full'>
+    <button className='bg-skin-button-primary text-skin-primary text-2xl font-bold my-2 py-3 px-10 rounded-full hover:text-white hover:shadow-[inset_13rem_0_0_0] hover:shadow-orange-600 duration-[400ms,700ms] transition-[color,box-shadow]'>
       Primary
     </button>
   );
--- client/src/components/buttons/SecondaryBtn.jsx
+++ client/src/components/buttons/SecondaryBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SecondaryBtn() {
   return (
-    <button className='bg-skin-button-secondary text-skin-secondary text-2xl font-bold my-2 py-3 px-10 rounded-full mx-6 '>
+    <button className='bg-skin-button-secondary text-skin-secondary text-2xl font-bold my-2 py-3 px-10 rounded-full mx-6 hover:scale-110 duration-500'>
       Secondary
     </button>
   );
