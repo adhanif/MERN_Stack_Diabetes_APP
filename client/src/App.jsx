fc6a9668b5c38e66c94f76474da7e1dd052af988
Anne <apohlmann2202@gmail.com>
2023-07-17 12:20:25 +0200

    navbar start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className='text-4xl font-bold underline '>Hello world!</h1>
+      <Navbar />
       <Hero />
       <Test theme='theme-secondary' />
       <Test theme='theme-3' />
