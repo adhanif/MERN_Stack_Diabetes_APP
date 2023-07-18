5ce841e15dbfcda83837985c272325c6c968cddb
ad.hanif <ad.hanif@outlook.com>
2023-07-18 12:41:00 +0200

    backend authentican for user completed, login form started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,11 +21,11 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
 
-      <h1 className='text-4xl font-bold underline '>Hello world!</h1>
+      {/* <h1 className='text-4xl font-bold underline '>Hello world!</h1>
       <Hero />
       <Test theme='theme-secondary' />
       <Test theme='theme-3' />
-      <Test theme='theme-4' />
+      <Test theme='theme-4' /> */}
 
     </>
   );
