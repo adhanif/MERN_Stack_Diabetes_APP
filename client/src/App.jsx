b512e51c1aa151f429aeca5d15483a935ff7077e
Anne <apohlmann2202@gmail.com>
2023-07-19 11:48:43 +0200

    nav and hero in one

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+//import { useState } from 'react';
+import { Route, Routes, NavLink } from "react-router-dom";
 import './App.css';
-import Hero from './components/Hero';
+import Hero2 from './components/Hero2';
+import What from './components/Example';
 import Test from './components/Test';
 import Navbar from './components/navbar';
+import Example from './components/Example';
+import Simple from './components/Simple';
 
 function App() {
   return (
     <>
-      <h1 className='text-4xl font-bold underline '>Hello world!</h1>
-      <Navbar />
-      <Hero />
-      <Test theme='theme-secondary' />
+      {/* <h1 className='text-4xl font-bold underline '>Hello world!</h1> */}
+      {/* <Hero2 /> */}
+      {/* <Navbar /> */}
+      <Example />
+      {/* <Simple /> */}
+      {/* <Test theme='theme-secondary' />
       <Test theme='theme-3' />
-      <Test theme='theme-4' />
+      <Test theme='theme-4' /> */}
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/articles" element={<h1>Articles</h1>} />
+        <Route path="/events" element={<h1>Events</h1>} />
+        <Route path="/about" element={<h1>About Us</h1>} />
+        <Route path="/login" element={<Test />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/components/Hero2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Background from '../images/sideSky.png';
+import Navbar from './navbar';
+import TypeOneLogo from '../images/TypeOne_white_noBG.png';
+
+export default function Hero2() {
+  return (
+    <div>
+      
+      {/* <img src={Background} alt='' className='h-screen w-screen'>
+            
+        </img> */}
+    </div>
+  );
+}
