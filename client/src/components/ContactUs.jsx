b020f0b4a47507aabd98f73335ac4cc26416e99a
elvisbevab <elvis.bevab@mailbox.org>
2023-07-19 17:02:37 +0200

    Error Messages included in Contact Form

--- client/src/components/ContactUs.jsx
+++ client/src/components/ContactUs.jsx
@@ -43,13 +43,19 @@ function ContactUs({ theme }) {
             Name
           </label>
           <input
-            {...register('name', { required: true })}
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Name is required',
+              },
+            })}
             className='shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-skin-base leading-tight focus:outline-none focus:shadow-outline'
             type='text'
             id='name'
             name='name'
             placeholder='Max Mustermann'
           />
+          <p>{errors.name?.message}</p>
 
           {/* Email */}
           <label
@@ -59,13 +65,19 @@ function ContactUs({ theme }) {
             Email
           </label>
           <input
-            {...register('email', { required: true })}
+            {...register('email', {
+              required: {
+                value: true,
+                message: 'Email is required to get an Answer',
+              },
+            })}
             className='shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-skin-base leading-tight focus:outline-none focus:shadow-outline'
             type='email'
             id='email'
             name='email'
             placeholder='max@mustermann.de'
           />
+          <p>{errors.email?.message}</p>
 
           {/* Subject */}
           <label
@@ -75,13 +87,19 @@ function ContactUs({ theme }) {
             Subject
           </label>
           <input
-            {...register('subject', { required: true })}
+            {...register('subject', {
+              required: {
+                value: true,
+                message: 'Subject is required',
+              },
+            })}
             className='shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-skin-base leading-tight focus:outline-none focus:shadow-outline '
             type='text'
             id='subject'
             name='subject'
             placeholder='Event'
           />
+          <p>{errors.subject?.message}</p>
 
           {/* Message */}
           <label
@@ -91,13 +109,20 @@ function ContactUs({ theme }) {
             Message
           </label>
           <textarea
-            {...register('message', { required: true })}
+            {...register('message', {
+              required: {
+                value: true,
+                message: 'We would like to know what you have to say',
+              },
+            })}
             className='shadow appearance-none border rounded w-full py-2 px-3 mb-6 text-skin-base leading-tight focus:outline-none focus:shadow-outline'
             name='message'
             id='message'
             cols='30'
             rows='5'
           ></textarea>
+          <p>{errors.message?.message}</p>
+
           <OutlineBtn text='Sign in with Google' />
           <SecondaryBtn text='Submit' />
         </form>
