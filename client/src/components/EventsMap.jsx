1cbb808663dd5905c50af3ab07c417466504c705
ad.hanif <ad.hanif@outlook.com>
2023-07-20 15:10:14 +0200

    react-leaflet map setup added in EventsMap component

--- client/src/components/EventsMap.jsx
+++ client/src/components/EventsMap.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 export default function EventsMap() {
+  const position = [51.505, -0.09];
   return (
-    <div>EventsMap</div>
-  )
+    <>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "80vh", width: "70%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 }
