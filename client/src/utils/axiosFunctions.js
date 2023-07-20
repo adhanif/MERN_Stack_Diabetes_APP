4ccf93b3d5d2926a14002d24c2436f629163b7cf
elvisbevab <elvis.bevab@mailbox.org>
2023-07-20 10:12:06 +0200

    contact form fixed

--- client/src/utils/axiosFunctions.js
+++ client/src/utils/axiosFunctions.js
@@ -1,7 +1,9 @@
 import axiosClient from '../axiosClient';
 
-export const postMessage = (props) => {
+export const postMessage = async (props) => {
   const { name, email, subject, message } = props;
   console.log(props, name);
+  try {
+  } catch (error) {}
   axiosClient.post('/message', props);
 };
