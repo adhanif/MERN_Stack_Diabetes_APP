6abebe436e3e95bd173867e78b46beb4ad8db77b
elvisbevab <elvis.bevab@mailbox.org>
2023-07-20 12:57:22 +0200

    event schema in backend added

---
+++ controllers/event.js
@@ -0,0 +1,19 @@
+const Event = require('../models/event');
+
+const addEvent = async (req, res) => {
+  return true;
+};
+
+const deleteEvent = async (req, res) => {
+  return true;
+};
+
+const getAllEvents = async (req, res) => {
+  return true;
+};
+
+const getEvent = async (req, res) => {
+  return true;
+};
+
+module.exports = { addEvent, deleteEvent, getAllEvents, getEvent };
---
+++ routes/event.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const eventRouter = express.Router();
+const {
+  addEvent,
+  deleteEvent,
+  getAllEvents,
+  getEvent,
+} = require('../controllers/event');
+
+eventRouter.get('/', getAllEvents);
+eventRouter.get('/:id', getEvent);
+eventRouter.post('/create', addEvent);
+eventRouter.delete('/:id', deleteEvent);
+
+module.exports = { eventRouter };
