eae53b8bf387e48d0c101714532920a21d993fe7
elvisbevab <elvis.bevab@mailbox.org>
2023-07-25 16:23:39 +0200

    footer done and custom breakpoints added for footer

---
+++ client/src/components/footerParts/StayInTouch.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function StayInTouch() {
+  return (
+    <div>
+      <h4 className='text-lg font-bold mb-2'>Stay in touch</h4>
+      <ul>
+        <li>TypeOne Diabetes GmbH</li>
+        <li>Hauptstraße 35</li>
+        <li>18182 Rostock</li>
+        <li>Phone: +49 171 43 849 34</li>
+      </ul>
+    </div>
+  );
+}
+
+export default StayInTouch;
