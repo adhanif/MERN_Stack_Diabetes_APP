b52a18369835115424cee918c7eb85723fc3f461
elvisbevab <elvis.bevab@mailbox.org>
2023-07-27 16:08:26 +0200

    padding in forms

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
       xxs: '360px',
@@ -11,48 +11,48 @@ export default {
     },
     extend: {
       fontSize: {
-        sm: "0.8rem",
-        base: "1rem",
-        xl: "1.25rem",
-        "2xl": "1.563rem",
-        "3xl": "1.953rem",
-        "4xl": "2.441rem",
-        "5xl": "3.052rem",
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
       },
       textColor: {
         thistle: 'var(--thistle)',
         denim: 'var(--denim)',
         skin: {
-          base: "var(--color-text-base)",
-          primary: "var(--color-text-primary)",
-          inverted: "var(--color-text-inverted)",
-          secondary: "var(--color-text-secondary)",
-          "outline-hover": "var(--color-text-outline-hover)",
-          "form-error": "var(--color-text-form-error)",
+          base: 'var(--color-text-base)',
+          primary: 'var(--color-text-primary)',
+          inverted: 'var(--color-text-inverted)',
+          secondary: 'var(--color-text-secondary)',
+          'outline-hover': 'var(--color-text-outline-hover)',
+          'form-error': 'var(--color-text-form-error)',
         },
       },
       backgroundColor: {
         skin: {
-          fill: "var(--color-fill)",
-          "button-primary": "var(--color-button-primary)",
-          "button-primary-hover": "var(--color-button-primary-hover)",
-          "button-secondary": "var(--color-button-secondary)",
-          "button-secondary-hover": "var(--color-button-secondary-hover)",
-          "button-outline": "var(--color-button-outline)",
-          "button-outline-hover": "var(--color-button-outline-hover)",
+          fill: 'var(--color-fill)',
+          'button-primary': 'var(--color-button-primary)',
+          'button-primary-hover': 'var(--color-button-primary-hover)',
+          'button-secondary': 'var(--color-button-secondary)',
+          'button-secondary-hover': 'var(--color-button-secondary-hover)',
+          'button-outline': 'var(--color-button-outline)',
+          'button-outline-hover': 'var(--color-button-outline-hover)',
         },
       },
       borderColor: {
         skin: {
-          "border-outline": "var(--color-border-outline)",
-          "border-outline-hover": "var(--color-border-outline-hover)",
+          'border-outline': 'var(--color-border-outline)',
+          'border-outline-hover': 'var(--color-border-outline-hover)',
         },
       },
       colors: {
-        lilac: "#BDB2C9",
-        whiteGrey: "#F4F4F4",
-        mint: "#B8C4B9",
-        grey: "#383740",
+        lilac: '#BDB2C9',
+        whiteGrey: '#F4F4F4',
+        mint: '#B8C4B9',
+        grey: '#383740',
       },
     },
   },
