191c6922f4f4ded556f9855c9fd4f2e11e3d25e7
ad.hanif <ad.hanif@outlook.com>
2023-07-28 15:43:12 +0200

    map buuton on eventcard implemented

--- client/src/components/AllEvents.jsx
+++ client/src/components/AllEvents.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import EventCard from './EventCard';
-import image from '../images/28998.jpg';
-import axiosClient from '../axiosClient';
-import FilterEvent from '../components/FilterEvent';
-import EventMapModal from '../components/EventMapModal';
-import { useForm } from 'react-hook-form';
-import SecondaryBtn from './buttons/SecondaryBtn';
+import { useState, useEffect } from "react";
+import EventCard from "./EventCard";
+import image from "../images/28998.jpg";
+import axiosClient from "../axiosClient";
+import FilterEvent from "../components/FilterEvent";
+import EventMapModal from "../components/EventMapModal";
+import { useForm } from "react-hook-form";
+import SecondaryBtn from "./buttons/SecondaryBtn";
 
 export default function AllEvents({ theme }) {
   const [events, setEvents] = useState([]);
-  const [path, setPath] = useState('/events');
+  const [path, setPath] = useState("/events");
 
   const {
     register,
@@ -17,9 +17,9 @@ export default function AllEvents({ theme }) {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      keyword: '',
-      distance: '',
-      categories: '',
+      keyword: "",
+      distance: "",
+      categories: "",
     },
   });
 
@@ -44,26 +44,26 @@ export default function AllEvents({ theme }) {
       <div
         className={`container flex flex-col mx-auto lg:flex-row space-y-5 space-x-0 lg:space-x-5 lg:space-y-0`}
       >
-        <aside className='w-full sm:w-2/3 lg:w-1/4'>
-          <div className='sticky top-0  w-full py-5'>
+        <aside className="w-full sm:w-2/3 lg:w-1/4">
+          <div className="sticky top-0  w-full py-5">
             <EventMapModal />
             <FilterEvent setPath={setPath} events={events} />
           </div>
         </aside>
-        <main role='main' className='w-full sm:w-2/3 md:w-3/4  px-2 pt-5'>
+        <main role="main" className="w-full sm:w-2/3 md:w-3/4  px-2 pt-5">
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className='flex space-x-5 mb-10 lg:w-4/5'
+            className="flex space-x-5 mb-10 lg:w-4/5"
           >
             <input
-              {...register('keyword', {
-                maxLength: { value: 30, message: 'Maximum length is 30' },
+              {...register("keyword", {
+                maxLength: { value: 30, message: "Maximum length is 30" },
               })}
-              type='text'
-              placeholder='Keyword'
-              className='shadow appearance-none border rounded w-full py-0 px-3 text-gray-700 dark:border-gray-600 leading-tight focus:outline-none  focus:ring-2 focus:border-blue-500 '
+              type="text"
+              placeholder="Keyword"
+              className="shadow appearance-none border rounded w-full py-0 px-3 text-gray-700 dark:border-gray-600 leading-tight focus:outline-none  focus:ring-2 focus:border-blue-500 "
             />
-            <SecondaryBtn text='Search' />
+            <SecondaryBtn text="Search" />
             {/* <button
               className='p-2 w-32   font-bold text-white rounded-full bg-black hover:bg-red-500  hover:scale-110  '
               type='submit'
@@ -76,12 +76,14 @@ export default function AllEvents({ theme }) {
             return (
               <EventCard
                 key={event._id}
-                image={event.image}
-                title={event.title}
-                info={event.eventInfo}
-                date={event.eventDate}
-                address={event.address}
-                categories={event.categories}
+                event={event}
+                // image={event.image}
+                // title={event.title}
+                // info={event.eventInfo}
+                // date={event.eventDate}
+                // address={event.address}
+                // categories={event.categories}
+                // location={event.location}
               />
             );
           })}
--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -1,46 +1,97 @@
-import React from 'react';
-import { MapPinIcon, CalendarDaysIcon } from '@heroicons/react/24/solid';
-export default function EventCard({
-  image,
-  title,
-  info,
-  date,
-  categories,
-  address,
-}) {
-  const eventDate = new Date(date).toLocaleDateString();
-
-  // const date = eventDate.toLocaleDateString();
+import React, { useState } from "react";
+import { MapPinIcon, CalendarDaysIcon } from "@heroicons/react/24/solid";
+import SecondaryBtn from "./buttons/SecondaryBtn";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import LocationMarker from "../components/LocationMarker";
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+export default function EventCard({ event }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const eventDate = new Date(event.date).toLocaleDateString();
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <>
-      <div className=' flex  bg-white flex-col md:flex-row border rounded-md overflow-hidden shadow-lg mb-5 lg:w-4/5'>
-        <div className=' md:w-2/5 px-6 py-6'>
+      <div className=" flex  bg-white flex-col md:flex-row border rounded-md overflow-hidden shadow-lg mb-5 lg:w-4/5">
+        <div className=" md:w-2/5 px-6 py-6">
           <img
-            className='w-full md:w-screen  h-56  rounded-lg'
-            src={image}
-            alt={title}
+            className="w-full md:w-screen  h-56  rounded-lg"
+            src={event.image}
+            alt={event.title}
           />
         </div>
 
-        <div className='px-6 py-4 md:pl-0  md:w-3/5'>
-          <div className='font-bold text-2xl mb-2'>{title}</div>
-          <p className='text-black text-base mb-2'>{info}</p>
-          <div className='flex flex-col md:flex-row md:space-x-20 '>
-            <div className='flex items-center space-x-2 mb-2 md:mb-0'>
-              <CalendarDaysIcon className='h-10 w-6 ' />
-              <p>{eventDate}</p>
+        <div className="px-6 py-4 md:pl-0  md:w-3/5">
+          <div className="font-bold text-2xl mb-2">{event.title}</div>
+          <p className="text-black text-base mb-2">{event.info}</p>
+          <div className="flex flex-col md:flex-row md:space-x-20 ">
+            <div className="flex items-center space-x-2 mb-2 md:mb-0">
+              <CalendarDaysIcon className="h-10 w-6 " />
+              <p>{event.eventDate}</p>
             </div>
-            <div className='flex items-center space-x-1'>
-              <MapPinIcon className='h-6 w-5 ' />
-              <p>{address}</p>
+            <div className="flex items-center space-x-1">
+              <MapPinIcon className="h-6 w-5 " />
+              <p>{event.address}</p>
             </div>
           </div>
+          {/* mapmodal event on map */}
+          <div>
+            <SecondaryBtn text="Show on map" onClick={handleModalToggle} />
+          </div>
+          {isModalOpen && (
+            <div
+              id="defaultModal"
+              tabIndex="-1"
+              aria-hidden="true"
+              className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full  h-full bg-gray-800 bg-opacity-75"
+            >
+              <div
+                className="relative bg-white rounded-lg shadow dark:bg-gray-700"
+                style={{ width: "80%" }}
+              >
+                <XMarkIcon
+                  className="h-8 w-8 cursor-pointer hover:bg-gray-200 hover:text-gray-900"
+                  onClick={handleModalToggle}
+                />
 
-          <div className=' pt-20 '>
-            {categories.map((category, index) => {
+                {/* MApcontainer */}
+                <MapContainer
+                  center={{ lat: 52.52, lng: 13.8 }}
+                  // center={position}
+                  zoom={13}
+                  scrollWheelZoom={true}
+                  style={{ height: "80vh", width: "100%" }}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+
+                  <Marker
+                    position={[
+                      event.location.coordinates[0],
+                      event.location.coordinates[1],
+                    ]}
+                    // position={[event.location.lat, event.location.long]}
+                  >
+                    <LocationMarker event={event} />
+                  </Marker>
+                </MapContainer>
+              </div>
+            </div>
+          )}
+          <div className=" pt-20 ">
+            {event.categories.map((category, index) => {
               return (
                 <span
-                  className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black cursor-pointer hover:scale-110 mr-2 mb-2'
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black cursor-pointer hover:scale-110 mr-2 mb-2"
                   key={index}
                 >
                   #{category}
