a721aeede2e62c015c91e1bd6af37c3d8fec9c4e
elvisbevab <elvis.bevab@mailbox.org>
2023-07-28 14:38:23 +0200

    navbar smaller

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
     <div id='navbarelvis' className=''>
       <header className=''>
         <nav
-          className='flex items-center justify-between p-6 lg:px-16'
+          className='flex items-center justify-between py-3 lg:px-16 px-6'
           aria-label='Global'
         >
           <div className='flex lg:flex-1'>
