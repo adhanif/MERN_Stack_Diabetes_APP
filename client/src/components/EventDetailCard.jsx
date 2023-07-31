3395d4b95405bc3f8ce68de8a58459bcdb35ea9b
ad.hanif <ad.hanif@outlook.com>
2023-08-01 00:00:09 +0200

    image, title, tags are added

--- client/src/components/EventDetailCard.jsx
+++ client/src/components/EventDetailCard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosClient from "../axiosClient";
 export default function EventDetailCard({ theme }) {
-  const [event, setEvent] = useState();
+  const [event, setEvent] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -10,41 +10,33 @@ export default function EventDetailCard({ theme }) {
       console.log(res.data);
       setEvent(res.data);
     });
-  }, []);
+  }, [id]);
 
   return (
     <div
       className={`${theme} bg-skin-fill shadow-2xl rounded-xl relative m-auto my-6 flex h-full w-[65%] flex-col bg-clip-border`}
     >
-      <div
-        className="relative bg-center flex h-96 max-w-[1200px] justify-center rounded-t-xl"
-        style={
-          {
-            //   backgroundImage:
-            //   backgroundRepeat: banner.repeat,
-            //   backgroundSize: banner.size,
-          }
-        }
-      >
-        {/* //Profile picture (separate div for future positioning) */}
-        {/* <div className='absolute -bottom-12 flex h-[95px] w-[95px] items-center justify-center rounded-full border-[4px] border-white'>
-      <img className='h-full w-full rounded-full' src={profilePic} alt='' />
-    </div> */}
+      <div className="relative bg-center flex h-96 max-w-[1200px] justify-center ">
+        <img src={event.image} alt={event.title} className="rounded-t-xl" />
       </div>
 
       {/* //General info */}
       <div className="mt-6 flex  px-4 gap-2 flex-col">
-        <h4 className="text-xl lg:text-2xl font-bold p-1">title</h4>
+        <h4 className="text-xl lg:text-2xl font-bold p-1 text-center">
+          {event.title}
+        </h4>
 
         <div className="flex gap-3 lg:text-lg p-1">
-          {/* {tags.map((tag, index) => (
-            <div
-              className="bg-gray-200 rounded-xl  px-3 py-0.5 border-2"
-              key={index}
-            >
-              {tag}
-            </div>
-          ))} */}
+          {event &&
+            event.categories &&
+            event.categories.map((category, index) => (
+              <div
+                className="bg-gray-200 rounded-xl  px-3 py-0.5 border-2 cursor-pointer hover:scale-110"
+                key={index}
+              >
+                #{category}
+              </div>
+            ))}
         </div>
 
         <div className="w-full flex justify-around p-1">
