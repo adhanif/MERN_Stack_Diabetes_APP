5abc2fd48d784bcdbf436dff6a287c55cf4f03cf
ad.hanif <ad.hanif@outlook.com>
2023-08-02 15:23:43 +0200

    final changes delete

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 // console.log(process.env.VITE_BE_URL);
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_BE_URL,
+  // baseURL: import.meta.env.VITE_BE_URL,
+  baseURL: (import.meta.env.VITE_BE_URL || "") + "/api",
   withCredentials: true,
 });
 
